da9642b34d3e259883b87402b1f6e5ddfa1f0364
Meghan Reading <mr3554@cumc.columbia.edu>
2017-11-15 16:03:00 -0500

    Update warfarin.sql

--- warfarin.sql
+++ warfarin.sql
@@ -1,7 +1,7 @@
 /*Because I was absent from class on this day I did not work in a group,
 so I chose one of the three queries to modify by myself*/
 
-/* Find find warfarin to find individuals on warfarin for the first time*/
+/*1. Find find warfarin to find individuals on warfarin for the first time*/
 SELECT person_id,min(drug_exposure_start_date),max_levels_of_separation, c.*
 FROM drug_exposure,concept_ancestor ca, concept c
 WHERE ca.ancestor_concept_id = 1310149 /* Warfarin */
