5bae5e07b9228b059e1aa8eb8ec5cb1d34268df2
Meghan Reading <mr3554@cumc.columbia.edu>
2017-11-15 16:02:13 -0500

    Add files via upload

--- warfarin.sql
+++ warfarin.sql
@@ -1,6 +1,10 @@
-/* Find other drugs that contains Warfarin AND Dabigratran */
-SELECT max_levels_of_separation, c.* 
-FROM concept_ancestor ca, concept c 
+/*Because I was absent from class on this day I did not work in a group,
+so I chose one of the three queries to modify by myself*/
+
+/* Find find warfarin to find individuals on warfarin for the first time*/
+SELECT person_id,min(drug_exposure_start_date),max_levels_of_separation, c.*
+FROM drug_exposure,concept_ancestor ca, concept c
 WHERE ca.ancestor_concept_id = 1310149 /* Warfarin */
-	AND ca.descendant_concept_id = c.concept_id 
+	AND ca.descendant_concept_id = c.concept_id
+GROUP BY person_id;
 ORDER BY max_levels_of_separation;
